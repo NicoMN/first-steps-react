d378cd27db5c8e7f6d84dfa800ea20cf1804b99c
NicoMN <nicolasmendes.19@hotmail.com>
2021-08-24 01:00:45 -0300

    Added basic React State usage

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,12 @@
+import { useState } from 'react';
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+
 function Todo(props) {
+const [ modalIsOpen, setModalIsOpen ] =  useState(false);
+
   function deleteHandler() {
-    console.log("Click!");
+    setModalIsOpen(true);
   }
 
   return (
@@ -11,8 +17,15 @@ function Todo(props) {
           Delete
         </button>
       </div>
+      { modalIsOpen && <Modal/> }
+      { modalIsOpen && <Backdrop/> }
+      
     </div>
   );
 }
 
 export default Todo;
+
+// { modalIsOpen ? <Modal/> : null}
+// modalIsOpen && <Modal/> Checks if modalIsOpen AND the function component Modal is true (Function Components are always true)
+// And returns the second value (Modal) if both are True
